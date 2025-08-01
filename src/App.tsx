ebb184f945ccfbebed4c077fdf27be9a4bc35a3d
difz <muhammadhifzhonharundoyo@mail.ugm.ac.id>
2025-08-01 19:11:36 +0800

    feat: add gallery page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ReservationPage from "./pages/ReservationPage";
+import GalleryPage from "./pages/GalleryPage";
 
 const App: React.FC = () => {
   return (
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/reservation" element={<ReservationPage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        {/* Add more routes as needed */}
       </Routes>
   );
 };
